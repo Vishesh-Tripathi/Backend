b03667b62cbf220a49e75d6c9b1300325dd65069
Vishesh Tripathi <tripathiishu467@gmail.com>
2024-07-09 16:26:15 +0530

    multer

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET
+});
+// Upload an file
+ const uploadCloudinary = async()=>{
+    try {
+        if(!localFilePath) return null; // checking if path exist
+        //upload
+       const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        // file uploaded
+        console.log("file uploaded successfully",response.url)
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally save temprory file as the upload operation get  failed
+        return null
+        
+    }
+ }
+
+export {uploadCloudinary}
