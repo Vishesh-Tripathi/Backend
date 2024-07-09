095ad9757c476a7a972facf8e0b280de5b2a72b3
Vishesh Tripathi <tripathiishu467@gmail.com>
2024-07-09 17:24:29 +0530

    all setUp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 //  require('dotenv').config( {path : './env'})
+
 import dotenv from "dotenv"
 import DBconnection from "./db/index.js"
+import { app } from "./app.js"
 dotenv.config({
     path:"./env"
 })
